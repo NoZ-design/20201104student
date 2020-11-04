23cb2eceba2dd26df678f91ac5614cf36028dde5
NoZ-design <68496572+NoZ-design@users.noreply.github.com>
2020-11-04 12:55:15 +0800

    20201104student

---
+++ 20201104student/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _20201104student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+
+            Console.WriteLine("Enter your name :" );
+            s.Name = Console.ReadLine();
+            Console.WriteLine("Enter your grade  :");
+            s.Grade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Student ID :");
+            s.StudentID =Console.ReadLine();
+
+            Console.WriteLine(s.Say());
+        }
+    }
+}
---
+++ 20201104student/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace _20201104student
+{
+    class Student
+    {
+        public string StudentID;
+        public string Name;
+        public int Grade;
+
+        public string Say()
+        {
+     
+            return "My name is " + Name + ",and I am a grade " + Grade + " student.My student ID is " + StudentID + ".";
+        }
+
+        
+    }
+}
